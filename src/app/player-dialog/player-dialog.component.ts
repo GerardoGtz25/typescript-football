7864cc31f6a46573aefd5331ffc8c9829eccd197
Gerardo Gutierrez <ggutierrez@grainchian.io>
2019-06-04 16:38:49 -0500

    Implementacion de modal

---
+++ src/app/player-dialog/player-dialog.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { Countries, SquadNumber } from '../interfaces/player';
+import { PlayerService } from '../services/player.service';
+import { TeamService } from '../services/team.service';
+import { take } from 'rxjs/operators';
+import { Team } from '../interfaces/team';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-player-dialog',
+  templateUrl: './player-dialog.component.html',
+  styleUrls: ['./player-dialog.component.sass']
+})
+export class PlayerDialogComponent implements OnInit {
+  private team;
+  public countries = Object.keys(Countries).map(key => ({ label: key, key: Countries[key] }));
+  public squadNumber = Object.keys(SquadNumber)
+  .slice(Object.keys(SquadNumber).length / 2)
+  .map(key => ({
+    label: key,
+    key: SquadNumber[key]
+  }));
+  constructor(private playerService: PlayerService, private teamService: TeamService) { }
+
+  ngOnInit() {
+    this.teamService
+      .getTeams()
+      .pipe(take(1))
+      .subscribe(teams => {
+        if (teams.length === 0) {
+          const team: Team = {
+            name: "MyAmazingTeam",
+            country: Countries.Mexico,
+            players: null
+          };
+          this.teamService.addTeam(team);
+        }
+      });
+  }
+
+  private newPlayer(playerFormValue) {
+    const key = this.playerService.addPlayer(playerFormValue).key;
+    const playerFormValueKey = {
+      ...playerFormValue,
+      key
+    };
+    const formattedTeam = {
+      ...this.team,
+      players: [...(this.team.players ? this.team.players : []), playerFormValueKey]
+    };
+    this.teamService.editTeam(formattedTeam);
+  }
+
+  onSubmit(playerForm: NgForm){
+    const playerFormValue = { ...playerForm.value };
+    if (playerFormValue.valid) {
+      playerFormValue.leftFooted = playerFormValue.leftFooted === '' ? false : playerFormValue.leftFooted;
+    }
+    this.newPlayer(playerFormValue);
+    window.location.replace('#');
+  }
+
+}
